4dcea3dd0e12db628d2038f624736db2ec897ab4
Niklas Berglund <niklas@asdasd.net>
2017-02-01 18:33:11 +0800

    >80% instead of 90%
    Since some special characters belong to both Chinese and Japanese. As
    of now they do not count as Chinese.

--- StringChineseTests/StringChineseTestsTests/StringChineseTestsTests.swift
+++ StringChineseTests/StringChineseTestsTests/StringChineseTestsTests.swift
@@ -25,8 +25,8 @@ class StringChineseTestsTests: XCTestCase {
         // Excerpt from http://www.gutenberg.org/cache/epub/25328/pg25328.html
         let text = "第六則 大和尚假意超昇 是日也，天朗氣清，涼風洊至。只見棚上豆花開遍，中間卻有幾枝，結成蓓蓓蕾蕾相 似許多豆莢。那些孩子看見嚷道：『好了，上邊結成豆了。』棚下就有人伸頭縮頸將 要彩他。眾人道：『新生豆莢是難得的。』主人道：『待我彩他下來，先煮熟了。今 日有人說得好故事的，就請他吃。』眾人道：『有理，有理。』棚下襬著一張椅子， 中間走出一個少年道：『今日待我坐在椅上，說個世情中有最不服人的一段話頭，叫 列位聽了猛然想著也要痛恨起來。"
         
-        XCTAssert(text.chinesePercentage() > 90.0, "Chinese text identified as >90% Chinese")
         XCTAssert(text.isChinese() == false, "Chinese text not identified as 100% Chinese")
+        XCTAssert(text.chinesePercentage() > 80.0, "Chinese text identified as >80% Chinese")
     }
     
     func testEnglishText() {
