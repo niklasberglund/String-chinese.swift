333528fa82ed2a6802add7fba0b0bb49b9a250f4
Niklas Berglund <niklas@asdasd.net>
2017-02-01 17:58:53 +0800

    Created project for unit tests

---
+++ StringChineseTests/StringChineseTestsTests/StringChineseTestsTests.swift
@@ -0,0 +1,23 @@
+//
+//  StringChineseTestsTests.swift
+//  StringChineseTestsTests
+//
+//  Created by Niklas Berglund on 2017-02-01.
+//  Copyright © 2017 Klurig. All rights reserved.
+//
+
+import XCTest
+@testable import StringChineseTests
+
+class StringChineseTestsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+}
